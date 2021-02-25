7b9cd3bb828e33ae75036142c4e80ea00851b98e
Trung Nguyen <trung1704ptit@gmail.com>
2021-02-25 16:15:27 +0700

    add training and testing phase

--- main.py
+++ main.py
@@ -24,19 +24,20 @@ from matplotlib import pyplot as plt
 
 import numba
 import cv2
-# import ast
-# from glob import glob
-# from numba import jit
+import ast
+from glob import glob
+from numba import jit
 from typing import List, Union, Tuple
 
 # Inputs required fro training
-DIR_INPUT='dataset'
-DIR_TRAIN=f'{DIR_INPUT}/train'
-DIR_TEST=f'{DIR_INPUT}/test'
+DIR_INPUT = 'dataset'
+DIR_TRAIN = f'{DIR_INPUT}/train'
+DIR_TEST = f'{DIR_INPUT}/test'
 
-model_path ='fasterrcnn_resnet50_fpn.pth' # Path for the best model to be saved
+# Path for the best model to be saved
+model_path = 'fasterrcnn_resnet50_fpn.pth'
 
-es_patience = 2 # this is required for early stopping, the number of epochs we will wait no improvement before stopping
+es_patience = 2  # this is required for early stopping, the number of epochs we will wait no improvement before stopping
 
 ## Reading box coordinates form train.csv
 train_df = pd.read_csv(f'{DIR_INPUT}/train.csv')
@@ -47,13 +48,17 @@ train_df['Y'] = -1
 train_df['w'] = -1
 train_df['h'] = -1
 
+
 def expand_bbox(x):
-    r = np.array(re.findall("([0-9]+[.]?[0-9]*)", x)) # find all float number for each row
+    # find all float number for each row
+    r = np.array(re.findall("([0-9]+[.]?[0-9]*)", x))
     if len(r) == 0:
         r = [-1, -1, -1, -1]
     return r
 
-train_df[['x', 'y', 'w', 'h']] = np.stack(train_df['bbox'].apply(lambda x: expand_bbox(x)))
+
+train_df[['x', 'y', 'w', 'h']] = np.stack(
+    train_df['bbox'].apply(lambda x: expand_bbox(x)))
 train_df.drop(columns=['bbox'], inplace=True)
 train_df['x'] = train_df['x'].astype(np.float)
 train_df['y'] = train_df['y'].astype(np.float)
@@ -61,12 +66,522 @@ train_df['w'] = train_df['w'].astype(np.float)
 train_df['h'] = train_df['h'].astype(np.float)
 
 ## Create training and validation datasets
-image_ids = train_df['image_id'].unique() # collection all unique images
+image_ids = train_df['image_id'].unique()  # collection all unique images
 valid_ids = image_ids[-665:]
 train_ids = image_ids[:-665]
 
-valid_df = train_df[train_df['image_id'].isin(valid_ids)]  
+valid_df = train_df[train_df['image_id'].isin(valid_ids)]
 train_df = train_df[train_df['image_id'].isin(train_ids)]
 
 print(valid_df.shape, train_df.shape)
 
+
+class WheatDataset(Dataset):
+    def __init__(self, dataframe, image_dir, transforms=None):
+        super().__init__()
+
+        self.image_ids = dataframe['image_id'].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+
+        image_id = self.image_ids[index]
+        # Getting all coordinates for the given image
+        records = self.df[self.df['image_id'] == image_id]
+
+        image = cv2.imread(
+            f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        boxes = records[['x', 'y', 'w', 'h']].values
+        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = torch.as_tensor(area, dtype=torch.float32)
+
+        # there is only one class
+        labels = torch.ones((records.shape[0],), dtype=torch.int64)
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        # target['masks'] = None
+        target['image_id'] = torch.tensor([index])
+        target['area'] = area
+        target['iscrowd'] = iscrowd
+
+        if self.transforms:
+            sample = {
+                'image': image,
+                'bboxes': target['boxes'],
+                'labels': labels
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+            target['boxes'] = torch.stack(
+                tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0)
+
+        return image, target, image_id
+
+    def __len__(self) -> int:
+        return self.image_ids.shape[0]
+
+
+# Albumentation
+def get_train_transform():
+    return A.Compose([
+        A.Flip(0.5),
+        ToTensorV2(p=1.0)
+    ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+def get_valid_transform():
+    return A.Compose([
+        ToTensorV2(p=1.0)
+    ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
+
+
+## Model - Transfer learning
+# load a model; pre-trained on COCO
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+# Modifying model for our task
+num_classes = 2  # 1 class (wheat) + background
+
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+
+# Helper functions for training
+class Averager:
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+
+## DataLoader
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+train_dataset = WheatDataset(train_df, DIR_TRAIN, get_train_transform())
+valid_dataset = WheatDataset(valid_df, DIR_TRAIN, get_valid_transform())
+
+
+# split the dataset in train and test set
+indices = torch.randperm(len(train_dataset)).tolist()
+
+train_data_loader = DataLoader(
+    train_dataset,
+    batch_size=8,
+    shuffle=False,
+    num_workers=4,
+    collate_fn=collate_fn
+)
+
+valid_data_loader = DataLoader(
+    valid_dataset,
+    batch_size=8,
+    shuffle=False,
+    num_workers=4,
+    collate_fn=collate_fn
+)
+
+
+# IOU calculation
+@jit(nopython=True)
+def calculate_iou(gt, pr, form='pascal_voc') -> float:
+    """Calculates the Intersection over Union.
+
+    Args:
+        gt: (np.ndarray[Union[int, float]]) coordinates of the ground-truth box
+        pr: (np.ndarray[Union[int, float]]) coordinates of the prdected box
+        form: (str) gt/pred coordinates format
+            - pascal_voc: [xmin, ymin, xmax, ymax]
+            - coco: [xmin, ymin, w, h]
+    Returns:
+        (float) Intersection over union (0.0 <= iou <= 1.0)
+    """
+    if form == 'coco':
+        gt = gt.copy()
+        pr = pr.copy()
+
+        gt[2] = gt[0] + gt[2]
+        gt[3] = gt[1] + gt[3]
+        pr[2] = pr[0] + pr[2]
+        pr[3] = pr[1] + pr[3]
+
+    # Calculate overlap area
+    dx = min(gt[2], pr[2]) - max(gt[0], pr[0]) + 1
+    
+    if dx < 0:
+        return 0.0
+    
+    dy = min(gt[3], pr[3]) - max(gt[1], pr[1]) + 1
+
+    if dy < 0:
+        return 0.0
+
+    overlap_area = dx * dy
+
+    # Calculate union area
+    union_area = (
+            (gt[2] - gt[0] + 1) * (gt[3] - gt[1] + 1) +
+            (pr[2] - pr[0] + 1) * (pr[3] - pr[1] + 1) -
+            overlap_area
+    )
+
+    return overlap_area / union_area
+
+@jit(nopython=True)
+def find_best_match(gts, pred, pred_idx, threshold = 0.5, form = 'pascal_voc', ious=None) -> int:
+    """Returns the index of the 'best match' between the
+    ground-truth boxes and the prediction. The 'best match'
+    is the highest IoU. (0.0 IoUs are ignored).
+
+    Args:
+        gts: (List[List[Union[int, float]]]) Coordinates of the available ground-truth boxes
+        pred: (List[Union[int, float]]) Coordinates of the predicted box
+        pred_idx: (int) Index of the current predicted box
+        threshold: (float) Threshold
+        form: (str) Format of the coordinates
+        ious: (np.ndarray) len(gts) x len(preds) matrix for storing calculated ious.
+
+    Return:
+        (int) Index of the best match GT box (-1 if no match above threshold)
+    """
+    best_match_iou = -np.inf
+    best_match_idx = -1
+
+    for gt_idx in range(len(gts)):
+        
+        if gts[gt_idx][0] < 0:
+            # Already matched GT-box
+            continue
+        
+        iou = -1 if ious is None else ious[gt_idx][pred_idx]
+
+        if iou < 0:
+            iou = calculate_iou(gts[gt_idx], pred, form=form)
+            
+            if ious is not None:
+                ious[gt_idx][pred_idx] = iou
+
+        if iou < threshold:
+            continue
+
+        if iou > best_match_iou:
+            best_match_iou = iou
+            best_match_idx = gt_idx
+
+    return best_match_idx
+
+@jit(nopython=True)
+def calculate_precision(gts, preds, threshold = 0.5, form = 'coco', ious=None) -> float:
+    """Calculates precision for GT - prediction pairs at one threshold.
+
+    Args:
+        gts: (List[List[Union[int, float]]]) Coordinates of the available ground-truth boxes
+        preds: (List[List[Union[int, float]]]) Coordinates of the predicted boxes,
+               sorted by confidence value (descending)
+        threshold: (float) Threshold
+        form: (str) Format of the coordinates
+        ious: (np.ndarray) len(gts) x len(preds) matrix for storing calculated ious.
+
+    Return:
+        (float) Precision
+    """
+    n = len(preds)
+    tp = 0
+    fp = 0
+    
+    # for pred_idx, pred in enumerate(preds_sorted):
+    for pred_idx in range(n):
+
+        best_match_gt_idx = find_best_match(gts, preds[pred_idx], pred_idx,
+                                            threshold=threshold, form=form, ious=ious)
+
+        if best_match_gt_idx >= 0:
+            # True positive: The predicted box matches a gt box with an IoU above the threshold.
+            tp += 1
+            # Remove the matched GT box
+            gts[best_match_gt_idx] = -1
+
+        else:
+            # No match
+            # False positive: indicates a predicted box had no associated gt box.
+            fp += 1
+
+    # False negative: indicates a gt box had no associated predicted box.
+    fn = (gts.sum(axis=1) > 0).sum()
+
+    return tp / (tp + fp + fn)
+
+
+@jit(nopython=True)
+def calculate_image_precision(gts, preds, thresholds = (0.5, ), form = 'coco') -> float:
+    """Calculates image precision.
+
+    Args:
+        gts: (List[List[Union[int, float]]]) Coordinates of the available ground-truth boxes
+        preds: (List[List[Union[int, float]]]) Coordinates of the predicted boxes,
+               sorted by confidence value (descending)
+        thresholds: (float) Different thresholds
+        form: (str) Format of the coordinates
+
+    Return:
+        (float) Precision
+    """
+    n_threshold = len(thresholds)
+    image_precision = 0.0
+    
+    ious = np.ones((len(gts), len(preds))) * -1
+    # ious = None
+
+    for threshold in thresholds:
+        precision_at_threshold = calculate_precision(gts.copy(), preds, threshold=threshold,
+                                                     form=form, ious=ious)
+        image_precision += precision_at_threshold / n_threshold
+
+    return image_precision
+
+
+models = [model]
+from ensemble_boxes import *
+
+#device = torch.device('cuda:0')
+
+def make_ensemble_predictions(images):
+    images = list(image.to(device) for image in images)    
+    result = []
+    for net in models:
+        net.eval()
+        outputs = net(images)
+        result.append(outputs)
+    return result
+
+def run_wbf(predictions, image_index, image_size=1024, iou_thr=0.55, skip_box_thr=0.5, weights=None):
+    boxes = [prediction[image_index]['boxes'].data.cpu().numpy()/(image_size-1) for prediction in predictions]
+    scores = [prediction[image_index]['scores'].data.cpu().numpy() for prediction in predictions]
+    labels = [np.ones(prediction[image_index]['scores'].shape[0]) for prediction in predictions]
+    boxes, scores, labels = weighted_boxes_fusion(boxes, scores, labels, weights=None, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
+    boxes = boxes*(image_size-1)
+    return boxes, scores, labels
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+#device = torch.device('cpu')
+
+model.to(device)
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+lr_scheduler = None
+
+num_epochs = 10
+
+loss_hist = Averager()
+best_val = None
+patience = es_patience
+for epoch in range(num_epochs):
+    start_time = time.time()
+    itr = 1
+    loss_hist.reset()
+    model.train()
+    for images, targets, image_ids in train_data_loader:
+        
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) if k =='labels' else v.float().to(device) for k, v in t.items()} for t in targets]#[{k: v.double().to(device) if k =='boxes' else v.to(device) for k, v in t.items()} for t in targets]
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+        loss_value = losses.item()
+
+        loss_hist.send(loss_value)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        if itr % 50 == 0:
+            print(f"Iteration #{itr} loss: {loss_value}")
+
+        itr += 1
+    
+    # update the learning rate
+    if lr_scheduler is not None:
+        lr_scheduler.step()
+
+    
+    #At every epoch we will also calculate the validation IOU
+    validation_image_precisions = []
+    iou_thresholds = [x for x in np.arange(0.5, 0.76, 0.05)]
+    model.eval()
+    for images, targets,imageids in valid_data_loader: #return image, target, image_id
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) if k =='labels' else v.float().to(device) for k, v in t.items()} for t in targets]
+        #outputs = model(images) 
+        
+        predictions = make_ensemble_predictions(images)
+   
+        for i, image in enumerate(images):
+            boxes, scores, labels = run_wbf(predictions, image_index=i)
+            boxes = boxes.astype(np.int32).clip(min=0, max=1023)
+            
+            preds = boxes#outputs[i]['boxes'].data.cpu().numpy()
+            #scores = outputs[i]['scores'].data.cpu().numpy()
+            preds_sorted_idx = np.argsort(scores)[::-1]
+            preds_sorted = preds[preds_sorted_idx]
+            gt_boxes = targets[i]['boxes'].cpu().numpy().astype(np.int32)
+            image_precision = calculate_image_precision(preds_sorted,
+                                                    gt_boxes,
+                                                    thresholds=iou_thresholds,
+                                                    form='coco')
+
+            validation_image_precisions.append(image_precision)
+    val_iou = np.mean(validation_image_precisions)
+    print(f"Epoch #{epoch+1} loss: {loss_hist.value}","Validation IOU: {0:.4f}".format(val_iou),"Time taken :",str(datetime.timedelta(seconds=time.time() - start_time))[:7])
+    if not best_val:
+        best_val = val_iou  # So any validation roc_auc we have is the best one for now
+        print("Saving model")
+        torch.save(model, model_path)  # Saving the model
+        #continue
+    if val_iou >= best_val:
+        print("Saving model as IOU is increased from",best_val,"to",val_iou)
+        best_val = val_iou
+        patience = es_patience  # Resetting patience since we have new best validation accuracy
+        torch.save(model, model_path)  # Saving current best model torch.save(model.state_dict(), 'fasterrcnn_resnet50_fpn.pth')
+    else:
+        patience -= 1
+        if patience == 0:
+            print('Early stopping. Best Validation IOU: {:.3f}'.format(best_val))
+            break
+
+model = torch.load(model_path)
+
+test_df = pd.DataFrame()
+test_df['image_id']=np.array([path.split('/')[-1][:-4] for path in glob(f'{DIR_TEST}/*.jpg')])
+
+
+
+class WheatTestDataset(Dataset):
+
+    def __init__(self, dataframe, image_dir, transforms=None):
+        super().__init__()
+
+        self.image_ids = dataframe['image_id'].unique()
+        self.df = dataframe
+        self.image_dir = image_dir
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+
+        image_id = self.image_ids[index]
+        records = self.df[self.df['image_id'] == image_id]
+
+        image = cv2.imread(f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        if self.transforms:
+            sample = {
+                'image': image,
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+        return image, image_id
+
+    def __len__(self) -> int:
+        return self.image_ids.shape[0]
+
+def get_test_transform():
+    return A.Compose([
+        # A.Resize(512, 512),
+        ToTensorV2(p=1.0)
+    ])
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+test_dataset = WheatTestDataset(test_df, DIR_TEST, get_test_transform())
+
+test_data_loader = DataLoader(
+    test_dataset,
+    batch_size=4,
+    shuffle=False,
+    num_workers=4,
+    drop_last=False,
+    collate_fn=collate_fn
+)
+
+model.eval()
+
+
+results = []
+outputs = []
+test_images = []
+for images, image_ids in test_data_loader:
+    images = list(image.to(device) for image in images)
+    predictions = make_ensemble_predictions(images)
+
+    for i, image in enumerate(images):
+        test_images.append(image) #Saving image values
+        boxes, scores, labels = run_wbf(predictions, image_index=i)
+
+        boxes = boxes.astype(np.int32).clip(min=0, max=1023)
+            
+        preds = boxes
+        preds_sorted_idx = np.argsort(scores)[::-1]
+        preds_sorted = preds[preds_sorted_idx]
+        boxes = preds
+        
+        output = {
+            'boxes': boxes,
+            'scores': scores
+        }
+
+        outputs.append(output) #Saving outputs and scores
+        image_id = image_ids[i]
+
+fig, axs = plt.subplots(2, 5, figsize=(32, 16))
+axs = axs.ravel()
+for i in range(10):
+    sample = test_images[i].permute(1,2,0).cpu().numpy()
+    boxes = outputs[i]['boxes']
+    scores = outputs[i]['scores']
+    boxes = boxes[scores >= detection_threshold].astype(np.int32)
+
+    for box in boxes:
+        cv2.rectangle(sample,
+                      (box[0], box[1]),
+                      (box[2], box[3]),
+                      (220, 0, 0), 2)
+
+    axs[i].set_axis_off()
+    axs[i].imshow(sample)
